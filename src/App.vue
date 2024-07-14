edab579908b42e40ce81f366ee9c59c1bfaa18ad
bcblevins <148902357+bcblevins@users.noreply.github.com>
2024-07-14 14:55:58 -0400

    Message bubbles done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router'
   import PetCard from './components/PetCard.vue';
+  import MessageBubble from './components/MessageBubble.vue';
 
 </script>
 
 <template>
-  <PetCard />
+  <div class="container">
+    <MessageBubble />
+  </div>
 </template>
 
 <style scoped>
-body {
-    background-color: #BDECFC;
-  }
+
+    .container {
+      background-color: #F1F7FF;
+      width: 90vw;
+      height: 90vh;
+      padding: 10vw;
+    }
+  
 </style>
---
+++ src/components/MessageBubble.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="message-bubble">
+        <span>
+            {{ message.body }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: {
+                "messageId": 1,
+                "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
+                "fromUsername": "cakelly4",
+                "toUsername": "bblevins96",
+                "testId": 1,
+                "patientId": 1
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+.message-bubble {
+    --bubble-color: #D5DFE2;
+    display: inline-block;
+    font-family: 'Trebuchet MS', sans-serif;
+    min-width: 4em;
+    width: auto;
+    max-width: 80%;
+    min-height: 2em;
+    background-color: var(--bubble-color);
+    color: #000;
+    border-radius: 2em;
+    padding: .7em;
+    padding-left: 1.2em;
+    box-shadow: 1px 1px 2px 1px rgb(162, 163, 163);
+}
+</style>

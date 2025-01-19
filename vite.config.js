c90e551ef1620b0775736583159bdca475ee8e45
Beau <beaucblevins@gmail.com>
2025-01-19 09:39:54 -0500

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
